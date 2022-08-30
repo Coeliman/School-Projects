7f8632c555eefd36e1cee26ab3451a735f015c36
Coeliman <coeliman24@gmail.com>
2022-08-30 11:47:05 -0600

    Made a controller that imma make control stuff

---
+++ Robotics/RoboticsHub.py
@@ -0,0 +1,8 @@
+import arduinoMorseCodetranslator
+print("Choose what you wish to continue to.")
+print("A. Text to Arduino C++ Morse Code")
+user_input = input().upper()
+if user_input == "A":
+    arduinoMorseCodetranslator.MORSE()
+else: 
+    print("Invalid input, quitting program")
