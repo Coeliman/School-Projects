31e0df4573f03365982203a448fd75f128bf7a20
Coeliman <Coeliman24@gmail.com>
2022-09-09 14:51:31 -0600

    The hub is beginning

---
+++ computer prog 1/!!!Prog1Hub.py
@@ -0,0 +1,7 @@
+import final_21, final_final, u1_cleopatraspool, u1_cookiedivider, u1_math
+import u2_catyearcalculator,u2_EvenorOdd,u2_highschoolgradecalc,u2_Multiusecalculator,u2_Rockpaperscissor,u2_RookMove,u2_Tipcalculator,u2_unit2scartch
+import u3_Guessgame,u3_rockpaperscizzorsv2,u3_tictactoe,u3_unit3scratch
+import u4_6sideddie,u4_customfunstest,u4_randnums,u4_unit4scrath,u4_visdicemeetsreqs
+import u5_drawshapeuser,u5_maze,u5_olympicring,u5_pixelartcharmander,u5_pixeldraw,u5_scrath,u5_shapes,u5_sobemoji,u5_tetris,u5_turtlerace,u5_turtleracev2,u5_upattern
+import u6_Alienrace,u6_rpsAIv1,u6_RPSAiv2,u6_tictactoeAI,u4_6sideddie
+#HUB FOR ALL PROJECTS
